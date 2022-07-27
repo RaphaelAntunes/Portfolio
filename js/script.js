3a312e9fe38d2b8b3fd3fa6477506228c6ec3429
RaphaelAntunes <50173113+RaphaelAntunes@users.noreply.github.com>
2022-07-27 00:57:11 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ const target = document.querySelectorAll('[data-anime]');
 const animationClass = 'animate';
 
 function animeScroll() {
-    const windowTop = window.pageYOffset + ((window.innerHeight * 4) / 4);
+    const windowTop = window.pageYOffset + ((window.innerHeight * 4) / 3);
     target.forEach(function(element) {
         if ((windowTop) > element.offsetTop) {
 
